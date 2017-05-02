6338d604062f8b28118b3e4dd346d9ca7d52ef11
Marko Tošić <mtosic@gmail.com>
2017-05-02 15:55:20 +0200

    added sql script

---
+++ scripts/heroes-data.sql
@@ -0,0 +1,9 @@
+SET IDENTITY_INSERT [dbo].[Heroes] ON
+INSERT INTO [dbo].[Heroes] ([Id], [IsDead], [Name]) VALUES (1, 0, N'Tyrion Lannister')
+INSERT INTO [dbo].[Heroes] ([Id], [IsDead], [Name]) VALUES (2, 1, N'Arya Stark')
+INSERT INTO [dbo].[Heroes] ([Id], [IsDead], [Name]) VALUES (3, 0, N'Cersei Lannister')
+INSERT INTO [dbo].[Heroes] ([Id], [IsDead], [Name]) VALUES (4, 0, N'Jon Snow')
+INSERT INTO [dbo].[Heroes] ([Id], [IsDead], [Name]) VALUES (5, 1, N'Daenerys Targaryen')
+INSERT INTO [dbo].[Heroes] ([Id], [IsDead], [Name]) VALUES (6, 1, N'Bran Stark')
+INSERT INTO [dbo].[Heroes] ([Id], [IsDead], [Name]) VALUES (7, 0, N'Mellisandre')
+SET IDENTITY_INSERT [dbo].[Heroes] OFF
